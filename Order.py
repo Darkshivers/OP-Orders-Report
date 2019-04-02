12a13f330b46eb850a99422f3d671f9f0a66abf7
Matthew levin <mattlevin2011@gmail.com>
2019-04-02 20:47:59 +0100

    Updates and bug fixes
    + Seperated individual currencies into their own sections
    +Orderstateid now exports as the values shown on the oak partnership CMS
    +added unicodecsv dependency
    +Queries all orders from the previous month
    
    -Removed old writefile function
    
    Bug Fixes
    
    * Removed spaces exporting to the CSV data
    *Changed unicode type to be friendlier with excel
    *

--- Order.py
+++ Order.py
@@ -4,9 +4,10 @@ import json
 import csv
 import re
 import io
+import unicodecsv
+import os
 
-
-
+apikey = ""
 
 def getOrderList():
     url = "https://oak-partnership.co.uk/api/Orders/orders/{IncludeOnlyActiveOrders}"
@@ -40,129 +41,102 @@ allorders = json.loads(getOrderList())
 order_header = []
 order_body = [] 
 header_state = False
+count = 0
 
 
-
-def writefile():
-
-    order_body_length = len(order_body)
-    count = 0 
-    header = []
-    content = []
-    headerwritten = False
-
-    
-
-    with open("OPOrderList.csv","wb") as myfile:
-            wr = csv.writer(myfile,quoting=csv.QUOTE_ALL) 
-            for x in range(order_body_length):
-                if "orderSupplierInvoiceDetailID" in order_body[x]:
-                    count+= 1
-                    print("Checking Currency" )
-                    print "Grabbing Order: " + str(count)
-                    print " "
-                    header,content = buildOrder((getItemPrice(GetIndividualOrders(count))))
-
-                    if headerwritten is False:
-                        wr.writerow(header)
-                        headerwritten = True
-
-                    for i in range(len(content)):
-                        content[i] = content[i].encode('utf-8')
-                        
-                    print "Content Found!"
-                    wr.writerow(content)
 
 def writeCSV(header,content):
     
     global header_state
 
     if header_state == False:
-        f = open("OPReport.csv","w")
+        f = open("OPReport.csv","wb")
         f.close()
 
-
     for i in range(len(content)):
-        content[i] = content[i].encode('utf-8')
+        content[i] = content[i].lstrip()
 
     with open("OPReport.csv", "ab") as myfile:
-        wr = csv.writer(myfile,quoting=csv.QUOTE_ALL)
-
+        wr = unicodecsv.writer(myfile,quoting=csv.QUOTE_ALL, encoding='Windows-1252')
+    
         if header_state == False:
             wr.writerow(header)
             header_state = True
 
         wr.writerow(content)
-         
 
-
+    
 def removeTrash(value):
 
-    del_list = [' ','}', '{','\[','\]',',','"',"null"] 
+    del_list = ['  ','}', '{','\[','\]',',','"',"null"] 
+    currencyheader = False
 
     global Order_interations
     global order_header
+    global count
     order_header = []
+    
 
     value = value.splitlines()
-
-    arr_length = len(value)
+    individual_item = []
 
     
+    arr_length = len(value)
+    
+
     for x in range(arr_length):
 
         if "rewardProductOptionDetailID" in value[x]:
             value[x] = ""
             value[x+2] = ""
+
         elif "accountID" in value[x] or "comments" in value[x] or "isSpecialOffer" in value[x] or "orderDetails" in value[x] or "emailAddress" in value[x] or "image" in value[x] or "orderDetailID" in value[x] or "rewardProductID" in value[x] or "rewardProductOptionID" in value[x]:
             value[x] = ""
         
         else:
             value[x] = re.sub("|".join(del_list),"", value[x])
         if x <= 13 and not "":
-
             if value[x] != "":
-                order_header.append(value[x])
-           
+                value_whitespace_stripped = value[x].strip()
+                order_header.append(value_whitespace_stripped)
+   
         elif x > 13 and not "":
             if value[x] != "":
                 order_body.append(value[x])
-
-
         
-    print order_header
-    buildOrder()
+        
+
+    for i in range(len(value)):
+
+        if "orderSupplierInvoiceDetailID" in value[i]:
     
-def buildOrder():
-
-    order_body_length = len(order_body)
-
-    count = 0
-    single_item = ""
-
-    for x in range(order_body_length):
-        if "orderSupplierInvoiceDetailID" in order_body[x]:
             count+=1
-            print "The Count " + str(count)
-            single_item = getItemPrice(GetIndividualOrders(count))
-            
+            print "Counter is at" +  str(count)
+        
 
-            headers = []
-            content = []
+            individual_item = getItemPrice(GetIndividualOrders(count))
+            individual_item = checkOrderstate(individual_item)
+            buildOrder(individual_item)
+            currencyheader = False
+        
+       
+        
 
-            single_order_formatted = []
+def buildOrder(single_item):
 
-            single_order_formatted.extend(order_header[x] for x in [0,1,2,6,8])
-            single_order_formatted.extend(single_item)
+    headers = []
+    content = []
 
-            print single_order_formatted
-            for i in range(len(single_order_formatted)):
-                segmented_objects = single_order_formatted[i].split(':')
+    single_order_formatted = []
+    single_order_formatted.extend(order_header[x] for x in [0,1,2,7,8])
+    single_order_formatted.extend(single_item)
 
-                headers.append(segmented_objects[0])
-                content.append(segmented_objects[1])       
-
-    writeCSV(headers,content)
+    for i in range(len(single_order_formatted)):
+        segmented_objects = single_order_formatted[i].split(':')
+        headers.append(segmented_objects[0])
+        content.append(segmented_objects[1]) 
+    
+    writeCSV(headers, content)  
 
 def GetHeader():
 
@@ -205,6 +179,7 @@ def getItemPrice(order):
               
     print "Order is in Points: " + str(isPoints)
     print "Order is in Currency: " + str(isCurrency)
+    print str(ordercount) + " orders found!"
 
     for x in order_length:
         if "pointsSpent" in order[x]:
@@ -216,13 +191,15 @@ def getItemPrice(order):
             if isCurrency == True:
                 print "Converting Points value to currency..."
                 print " "
-                order[x] = str(itemcost[0]) + ":" + unichr(163) + str(float(itemcost[1]) / 100).encode('utf-8').strip()
+                order[x-1] = "currencySpent" + ":" + unichr(163) + str(float(itemcost[1]) / 100).encode('utf-8').strip()
+                order[x] = "pointsSpent: "
                 print order[x]
                 print " "
                 return order
             
             if isPoints == True:
                 print order[x]
+                order[x-1] = "currencySpent: "
                 return order
             else:
                 print "No Currency Found"
@@ -232,6 +209,7 @@ def GetIndividualOrders(order_number):
     orders = order_body
     single_order = []
     count = 0
+    sterling_object = False
 
     for i in range(len(orders)):
         if "orderSupplierInvoiceDetailID" in orders[i]:
@@ -242,6 +220,12 @@ def GetIndividualOrders(order_number):
             single_order.append(orders[i])
             if "orderSupplierInvoiceDetailID" in orders[i]:
                 count+= 1
+        
+    for i in range(len(single_order)):
+        if "pointsSpent" in single_order[i] and sterling_object == False:
+            single_order.insert(i,"currencySpent:0")
+            sterling_object = True
+
 
     return single_order
             
@@ -251,17 +235,56 @@ def getOrderAmount():
 def getEveryOrder():
     print 'Getting Oak Partnership Orders...'
     amount = getOrderAmount()
-    for x in range(20):
+    for x in range(40):
         print str(x) + ' Out of: ' +  str(amount)
         removeTrash(getMonthsOrders(x))
 
+    os.startfile("OPReport.csv")
+
 def getMonthsOrders(i):
+
+    today = datetime.date.today()
+    first = today.replace(day=1)
+    lastMonth = first - datetime.timedelta(days=1)
+    print lastMonth.strftime("%Y-%m")
+
+    datestring = str(lastMonth.strftime("%Y-%m"))
+
+    orderitem = ""
     current  = allorders[i]
     current  = str(current).split(",")
     orderno = str(current[7]).split(" ")
-    print orderno[2]
-    orderitem = getOrder(str(orderno[2]))
+    date = str(current[3]).split(" ")
+    if datestring != date[2]:
+        print orderno[2]
+        orderitem = getOrder(str(orderno[2]))
     return orderitem
+
+def checkOrderstate(individual_order):
+    state = ""
+    orderstatusid = "orderStatusID:"
+    print individual_order
+    for i in range(len(individual_order)):
+        if "orderStatusID" in individual_order[i]:
+            print "Order Status Found"
+            orderid = individual_order[i].split(":")
+            orderid = orderid[1].strip()
+
+            print orderid
+    
+            if "1" in orderid:
+                print "TO BE PROCESSED"
+                individual_order[i] = orderstatusid + str("Waiting to be processed")
+            if "2" in orderid:
+                individual_order[i] = (orderstatusid + str("Partitally Shipped"))
+            if "3" in orderid:
+                individual_order[i] = (orderstatusid + str("Ordered from supplier"))
+            if "4" in orderid:
+                individual_order[i] = (orderstatusid + str("Complete"))
+
+    return individual_order
+
+
 
 def GetJSON():
 
@@ -273,10 +296,9 @@ def GetJSON():
         current  = str(current).split(",")
         orderno = current[7].split(" ")
         date = str(current[3]).split(" ")
-        if '2019-02' in date[2]:
+        if '2019-02' != date[2]:
             amount += 1
             order_list.append(orderno[2])
-    print amount
 
     file = open("monthsOrders.txt","w")
 
@@ -299,7 +321,8 @@ def getMonthlyOrders():
     for i in range(len(allorders)):
         if datestring in allorders[i]:
             print allorders[i]
-         
+
+os.system("TASKKILL /F /IM EXCEL.exe")      
 getEveryOrder()
 
 
